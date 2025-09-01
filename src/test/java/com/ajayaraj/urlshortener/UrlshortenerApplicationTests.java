a40e1b8cd0d243763d5d68083fc1133dc6a69f9f
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-01 19:37:14 +0530

    Initial Commit

---
+++ src/test/java/com/ajayaraj/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajayaraj.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
