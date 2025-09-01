ae0900ca64ffe67761aec72f919561209081630b
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-01 22:11:02 +0530

    Changed the inMemory to MySQL data storage.

--- src/main/java/com/ajayaraj/urlshortener/controller/UrlShortenerController.java
+++ src/main/java/com/ajayaraj/urlshortener/controller/UrlShortenerController.java
@@ -1,13 +1,14 @@
 package com.ajayaraj.urlshortener.controller;
 
 import com.ajayaraj.urlshortener.model.UrlRequest;
+import com.ajayaraj.urlshortener.model.UrlStore;
+import com.ajayaraj.urlshortener.repo.UrlShortenerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -15,7 +16,8 @@ public class UrlShortenerController {
 
     private static final String BASE_URL = "http://localhost:8080/";
 
-    Map<String, String> urlMap = new HashMap<>();
+    @Autowired
+    private UrlShortenerRepo urlShortenerRepo;
 
     @PostMapping("/shorten")
     public ResponseEntity<String> shorten(@RequestBody UrlRequest request) {
@@ -24,7 +26,11 @@ public class UrlShortenerController {
 
         String shortUrl = UUID.randomUUID().toString().substring(0,6);
 
-        urlMap.put(shortUrl, longUrl);
+        UrlStore urlStore = new UrlStore();
+        urlStore.setLongUrl(longUrl);
+        urlStore.setShortUrl(shortUrl);
+
+        urlShortenerRepo.save(urlStore);
 
         return ResponseEntity.ok(BASE_URL + shortUrl);
     }
@@ -32,11 +38,13 @@ public class UrlShortenerController {
     @GetMapping("/{code}")
     public ResponseEntity<Void> getUrl(@PathVariable String code) {
 
-        if(!urlMap.containsKey(code)) {
+        UrlStore longUrl = urlShortenerRepo.findByShortUrl(code);
+
+        if(longUrl == null) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(urlMap.get(code))).build();
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(longUrl.getLongUrl())).build();
     }
 
 }
---
+++ src/main/java/com/ajayaraj/urlshortener/model/UrlStore.java
@@ -0,0 +1,41 @@
+package com.ajayaraj.urlshortener.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UrlStore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String shortUrl;
+    public String longUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+
+    public String getLongUrl() {
+        return longUrl;
+    }
+
+    public void setLongUrl(String longUrl) {
+        this.longUrl = longUrl;
+    }
+}
